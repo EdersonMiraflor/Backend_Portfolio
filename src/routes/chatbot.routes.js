d0f1b3f4a8db689bc0ebf3988d2a353c74ea2038
EdersonMiraflor <125487899+EdersonMiraflor@users.noreply.github.com>
2025-09-26 18:13:14 +0800

    Revert "Production"
    This reverts commit 778f2fc19802fbb887f806b1bc5e35fb665841aa.

--- src/routes/chatbot.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const chatbot_controller_1 = require("../controller/chatbot.controller");
-const router = (0, express_1.Router)();
-router.post('/ask', chatbot_controller_1.askModel);
-exports.default = router;
--- src/routes/email.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const email_controller_1 = require("../controller/email.controller");
-const router = (0, express_1.Router)();
-router.post('/send', email_controller_1.sendEmail);
-exports.default = router;
